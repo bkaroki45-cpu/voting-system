d39ba5d63242bda3f8e91300ed35b3ddfe59a41f
brian <bkaroki452gmail.com>
2026-03-27 19:10:42 +0300

    Update settings for Render deployment

--- voting/settings.py
+++ voting/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +136,4 @@ AUTHENTICATION_BACKENDS = [
 # Default primary key field type
 # ------------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
