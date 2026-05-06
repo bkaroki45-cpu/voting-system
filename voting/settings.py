8edc79eaf6d264386e63eefce7d7ed84f5f8e953
Brian Karoki <bkaroki45@gmail.com>
2026-05-06 13:21:07 +0300

    Update settings.py

--- voting/settings.py
+++ voting/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = 'voting.wsgi.application'
 # ------------------------------
 # Database
 # ------------------------------
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 # ------------------------------
