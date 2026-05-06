44bab5c0dc1dd8649d2fe8c851744f97e7a267ae
Brian Karoki <bkaroki45@gmail.com>
2026-05-06 13:27:14 +0300

    Update settings.py

--- voting/settings.py
+++ voting/settings.py
@@ -79,11 +79,21 @@ WSGI_APPLICATION = 'voting.wsgi.application'
 import dj_database_url
 import os
 
-DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
-    )
-}
+import dj_database_url
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # ------------------------------
 # Password validation
