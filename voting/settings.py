988bca8edfbb224069ecdcfbbc628ab9ede0b00d
Brian Karoki <bkaroki45@gmail.com>
2026-05-06 14:07:46 +0300

    Update settings.py

--- voting/settings.py
+++ voting/settings.py
@@ -81,19 +81,16 @@ import os
 
 import dj_database_url
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+import dj_database_url
+import os
 
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(DATABASE_URL)
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # ------------------------------
 # Password validation
