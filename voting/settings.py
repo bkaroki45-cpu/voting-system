9b48556d0cd50712553944488f296d7511f280b8
Brian Karoki <bkaroki45@gmail.com>
2026-05-06 15:08:08 +0300

    Update settings.py

--- voting/settings.py
+++ voting/settings.py
@@ -147,11 +147,4 @@ AUTHENTICATION_BACKENDS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-import os
 
-if os.environ.get('RENDER'):
-    from django.core.management import call_command
-    try:
-        call_command('migrate', interactive=False)
-    except Exception as e:
-        print("Migration error:", e)
