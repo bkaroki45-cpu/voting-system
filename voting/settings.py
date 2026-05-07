d111a836c781174511b603877c5879f3f2dcb7eb
Brian Karoki <bkaroki45@gmail.com>
2026-05-07 11:34:21 +0300

    Update settings.py

--- voting/settings.py
+++ voting/settings.py
@@ -87,12 +87,11 @@ WSGI_APPLICATION = 'voting.wsgi.application'
 # ------------------------------
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
 }
-
 # ------------------------------
 # Password validation
 # ------------------------------
