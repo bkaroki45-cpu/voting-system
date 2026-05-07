6031a7ba267d84304848b7c58d822149399bca0a
Brian Karoki <bkaroki45@gmail.com>
2026-05-07 08:58:35 +0300

    Update settings.py

--- voting/settings.py
+++ voting/settings.py
@@ -87,13 +87,10 @@ WSGI_APPLICATION = 'voting.wsgi.application'
 # Database
 # ------------------------------
 import dj_database_url
-import os
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
+        default=os.environ.get('DATABASE_URL')
     )
 }
 
